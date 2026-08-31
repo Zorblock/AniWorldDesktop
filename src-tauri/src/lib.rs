ced82219a2d40773945d0c5f9b5792829c6a0859
Zorblock <turtelmaster11@gmail.com>
2026-08-31 02:07:55 +0200

    a

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -196,6 +196,7 @@ pub fn run() {
             .title(APP_TITLE)
             .inner_size(1280.0, 800.0)
             .min_inner_size(900.0, 600.0)
+            .maximized(true)
             .decorations(false)
             .shadow(true)
             .center()
